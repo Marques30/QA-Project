48d2e400d4336bcfcf011bf3a7592f976bb7a7a3
Marques30 <marques.butilla@gmail.com>
2016-01-05 13:40:41 -0800

    Tables added and footer

--- action.js
+++ action.js
@@ -1,5 +1,5 @@
 function check(form) {
-  if (form.userid.value == "crash" && form.pswrd.value == "Bandicoot") {
+  if (form.userid.value == "crash" && form.pswrd.value == "x") {
     window.open('YU.html')
   } else {
     alert("Error incorrect Password or Username")
