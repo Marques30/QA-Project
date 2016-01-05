cf5e714dc5b1d8dd2eaf68ab3f5656e5cbf911e7
Marques30 <marques.butilla@gmail.com>
2016-01-05 13:29:51 -0800

    Links finished

--- action.js
+++ action.js
@@ -1,6 +1,6 @@
 function check(form) {
   if (form.userid.value == "crash" && form.pswrd.value == "Bandicoot") {
-    window.open('google.com')
+    window.open('YU.html')
   } else {
     alert("Error incorrect Password or Username")
   }
